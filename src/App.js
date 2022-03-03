f627f8bf645657cc32bcbf9f6d7437614df3a9a4
dmanibarnett <dmani.barnett@gmail.com>
2022-03-03 21:49:33 +0000

    Applied feedback comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import { Container, Heading, Image, VStack, Text, Box} from '@chakra-ui/react';
+import { Heading, Image, VStack, Text, Box, Container } from '@chakra-ui/react';
 import Card from './components/Card';
 import qrCode from './assets/image-qr-code.png'
 function App() {
   return (
-    <Container centerContent={true} justify="center" mt={70}>
-      <Card>
-        <VStack>
-          <Image src={qrCode} alt='QR Code' borderRadius='xl'></Image>
-          <Heading as='h1'>
-            Improve your front-end skills by building projects
-          </Heading>
-          <Text>
-            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-          </Text>
-        </VStack>
-      </Card>
-      <Box>
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>.
-        Coded by <a href="https://www.frontendmentor.io/profile/Dmani871">Dmani Barnett</a>.
-      </Box>
-    </Container>
+    <Card>
+      <VStack>
+        <Image src={qrCode} alt='QR Code' borderRadius='xl'></Image>
+        <Heading as='h1' size='md'>
+          Improve your front-end skills by building projects
+        </Heading>
+        <Text>
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </Text>
+      </VStack>
+    </Card>
   );
 }
 
--- src/styles/Card.js
+++ src/styles/Card.js
@@ -1,26 +1,28 @@
 const Card = {
-    // The styles all Cards have in common
-    baseStyle: {
-      display: 'flex',
-      flexDirection: 'column',
-      background: 'white',
-      alignItems: 'center',
-      gap: 6,
+  // The styles all Cards have in common
+  baseStyle: {
+    display: 'flex',
+    flexDirection: 'column',
+    background: 'white',
+    borderRadius: 'xl',
+    boxShadow: 'xl',
+    height: '25%',
+    width:'25%',
+    mx:"auto",
+    my:"25%"
+  },
+  // Two variants: rounded and smooth
+  variants: {
+    rounded: {
+      padding: 2,
       borderRadius: 'xl',
-      boxShadow: 'xl'
-    },
-    // Two variants: rounded and smooth
-    variants: {
-      rounded: {
-        padding: 8,
-        borderRadius: 'xl',
-        boxShadow: 'xl',
-      }
-    },
-    // The default variant value
-    defaultProps: {
-      variant: 'rounded',
-    },
-  }
+      boxShadow: 'xl',
+    }
+  },
+  // The default variant value
+  defaultProps: {
+    variant: 'rounded',
+  },
+}
 
-  export default Card;
+export default Card;
